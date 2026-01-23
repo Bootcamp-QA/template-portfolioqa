8cd9ec1f62ca718323654534985c0db8164149e4
Reyes Cuesta Bootcamp QA <154596444+bootcampqa@users.noreply.github.com>
2026-01-23 12:06:51 +0100

    Refactor script.js for clarity and organization

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
-// CONFIG SUPABASE
-//URL API y API KEY
+// 1. CAMBIAR POR URL Y KEY DE TU API
 const SUPABASE_URL = 'https://ifgzperqnxoomyvvytzr.supabase.co/rest/v1/forms';
 const SUPABASE_API_KEY = 'sb_publishable_s4C8y1rgb321cEF5B969MA_3jMwVpNJ';
 
 
-//OBTENER DATOS DEL FORMULARIO Y ENVIARLOS A LA API
+//ENVIAR DATOS DE UN FORMULARIO A UNA API CON POST
+//2. FUNCION PARA ENVIAR DATOS DEL FORMULARIO A LA API, esta funcion debes ponerla en el boton del formulario que quieras usar.
 function enviarFormulario(event) {
   event.preventDefault(); 
 
-  //Se extrae del formulario los datos a enviar a la api, incluyendo en primer lugar los campos de la api, y en segundo lugar el id del elemento del formulario de donde se reciben los datos.
+  //3. CAMBIA SI ES NECESARIO LOS DATOS, DEBES INDICAR EL NOMBRE DEL CAMPO QUE QUIERES MANDAR A LA API, Y EL ID DEL FORMULARIO QUE TIENE ESE DATO.
   var data = {
     email: document.getElementById('email').value, //campo email de la api, se obtiene del input del formulario con id email
     subject: document.getElementById('asunto').value, //campo subject de la api, se obtiene del input del formulario con id asunto
     message: document.getElementById('mensaje').value, //campo message de la api, se obtiene del input del formulario con id mensaje
     name: document.getElementById('nombre').value //campo name de la api, se obtiene del input del formulario con id nombre
-  };
-  //Funcion para enviar los datos a la api con POST.
+  }
   postAPI(data);
 
-   return false; // evita submit normal
+   return false;
 }
-//MOSTRAR MENSAJE SI SE HA ENVIADO BIEN LOS DATOS
+//4. CAMBIA SI QUIERES EL MENSAJE QUE MUESTRA AL ENVIAR EL FORMULARIO (OPCIONAL) 
 function mostrarMensajeExito(){
   var formMessage = document.getElementById('formMessage'); //id del elemento de la pagina donde se va a mostrar mensaje
   formMessage.textContent = "Formulario enviado correctamente"; //mensaje que va a mostrarse
   formMessage.style.color  = "green"; //color en el que se va a mostrar
 };
 
-//MOSTRAR MENSAJE SI SE HAN ENVIADO MAL LOS DATOS
+//5. CAMBIA SI QUIERES EL MENSAJE QUE MUESTRA AL ENVIAR EL FORMULARIO (OPCIONAL) 
 function mostrarMensajeError(){
   var formMessage = document.getElementById('formMessage'); //id del elemento de la pagina donde se va a mostrar el mensaje
   formMessage.textContent = "Error al enviar el formulario"; //mensaje que va a mostrarse
   formMessage.style.color  = "red"; //color en el que se va a mostrar
 };
 
-//FUNCION GENERICA PARA ENVIAR DATOS A UNA API POST Y MOSTRAR UN MENSAJE DE ERROR O EXITO.
+//FUNCION GENERICA PARA ENVIAR DATOS A UNA API POST Y MOSTRAR UN MENSAJE DE ERROR O EXITO. NO NECESITAS CAMBIAR NADA, FUNCIONA SIEMPRE.
 function postAPI (data){
   fetch(SUPABASE_URL, {
     method: 'POST',
@@ -58,31 +57,16 @@ function postAPI (data){
     console.log(error)
     mostrarMensajeError();
   });
+};
 
- 
-};
-  //FUNCIÓN PARA OBTENER LOS DATOS DE LA API CON GET
-function getAPI() {
-  return fetch(SUPABASE_URL, {
-    method: 'GET',
-    headers: {
-      'apikey': SUPABASE_API_KEY,
-      'Authorization': 'Bearer ' + SUPABASE_API_KEY
-    }
-  })
-  .then(function(response) {
-    if (!response.ok) throw new Error('Error al cargar');
-    return response.json();
-  });
-};
-// ----------------------------
-// FUNCION PARA MOSTRAR DATOS EN UNA PAGINA
-// ----------------------------
+//MOSTRAR DATOS EN UNA TABLA DE UNA API CON GET
+//1. USA ESTA FUNCION EN EL BOTON DE LA PAGINA EN LA QUE QUIERAS MOSTRAR LOS DATOS
 function mostrarDatos() {
- //INDICAR AQUI EL ID DE LA TABLA DONDE QUIERE MOSTRAR LOS DATOS
+ //2. INDICA EL ID DE LA TABLA DONDE QUIERES QUE SE MUESTRE
   const table = document.getElementById("formsTable");
   if (!table) return;
 
+  //LLAMA A LA API Y MUESTRA LOS DATOS EN LA TABLA. GENERICA. NO NECESITA CAMBIOS.
   getAPI()
     .then(function(data) {
 
@@ -129,5 +113,19 @@ function mostrarDatos() {
     });
 }
 
+  //FUNCIÓN PARA OBTENER LOS DATOS DE LA API CON GET. GENERICA NO NECESITA CAMBIOS.
+function getAPI() {
+  return fetch(SUPABASE_URL, {
+    method: 'GET',
+    headers: {
+      'apikey': SUPABASE_API_KEY,
+      'Authorization': 'Bearer ' + SUPABASE_API_KEY
+    }
+  })
+  .then(function(response) {
+    if (!response.ok) throw new Error('Error al cargar');
+    return response.json();
+  });
+};
 
 
