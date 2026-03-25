4bb7e474876e495ff52aa9bdfdf501ee05c5d693
Reyes Cuesta Bootcamp QA <154596444+bootcampqa@users.noreply.github.com>
2026-03-25 19:23:28 +0100

    Add SQL insert statements for form data

---
+++ sql/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO form (name, email, subject, age, message)
+VALUES
+('Ana', 'ana@gmail.com', 'information', NULL, 'hola testing'),
+('Pedro', 'luis@gmail.com', 'information', 29, 'hola testing'),
+('Luis', 'maria@gmail.com', 'information', 18, 'hola testing');
