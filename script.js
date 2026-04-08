c207e7f83526af901d0eeca1c43ac8176ee0e4de
Reyes Cuesta Bootcamp QA <mybootcampqa@gmail.com>
2026-04-08 19:07:42 +0200

    updated api form
    updated api form

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 1. CAMBIAR POR URL Y KEY DE TU API
-const SUPABASE_URL = 'https://ifgzperqnxoomyvvytzr.supabase.co/rest/v1/forms';
-const SUPABASE_API_KEY = 'sb_publishable_s4C8y1rgb321cEF5B969MA_3jMwVpNJ';
+const SUPABASE_URL = 'https://ekganubktuuqcbtubnwb.supabase.co/rest/v1/forms'; //URL de la API, en este caso es la url de supabase, pero puede ser cualquier API
+const SUPABASE_API_KEY = 'sb_publishable_9VMR2m2hkgpsoJ2AqK0yZg_ab7siGbi';
 
 
 //ENVIAR DATOS DE UN FORMULARIO A UNA API CON POST
